50bffef552613f7ec0c05a50fe4602af00b2e994
AhmedElshobaky <ahmedalycess@gmail.com>
2024-08-06 14:26:46 +0200

    feat (optim): add adam and sgd-with-momentum

--- Optimization/Optimizers.py
+++ Optimization/Optimizers.py
@@ -15,5 +15,38 @@ class Sgd:
     
     def calculate_update(self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray) -> np.ndarray:
         return weight_tensor - self.learning_rate * gradient_tensor
+
+class SgdWithMomentum:
+
+    def __init__(self, learning_rate: float, momentum_rate: float) -> None:
+        self.learning_rate = learning_rate
+        self.momentum_rate = momentum_rate
+        self.velocity = 0
+    
+    def calculate_update(self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray) -> np.ndarray:
+        self.velocity = self.momentum_rate * self.velocity - self.learning_rate * gradient_tensor
+        return weight_tensor + self.velocity
+    
+class Adam:
+
+    def __init__(self, learning_rate: float = 0.001, mu: float = 0.9, rho: float = 0.999) -> None:
+        self.learning_rate = learning_rate
+        self.mu = mu
+        self.rho = rho
+        self.v = 0
+        self.r = 0
+        self.k = 1 # bias correction term
+        self.epsilon = 1e-8
+
+    
+    def calculate_update(self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray) -> np.ndarray:
+
+        self.v = self.mu * self.v + (1 - self.mu) * gradient_tensor
+        self.r = self.rho * self.r + (1 - self.rho) * np.square(gradient_tensor)
+        v_hat = self.v / (1 - np.power(self.mu, self.k))
+        r_hat = self.r / (1 - np.power(self.rho, self.k))
+        self.k += 1
+        return weight_tensor - self.learning_rate * v_hat / (np.sqrt(r_hat) + self.epsilon)
+
     
 
